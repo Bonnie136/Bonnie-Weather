f988ba687943897c86f0fe647a8c81ede6d2334c
Bonnie136 <bonnie.wells@gmail.com>
2026-03-22 12:03:56 +0000

    Date update on form submit

--- SRC/index.js
+++ SRC/index.js
@@ -26,7 +26,39 @@ function formatDate(date) {
   return `${formattedDay} ${nowDate}, ${hours}:${minutes}`;
 }
 
+function updateDate() {
+  let date = new Date();
+  let day = date.getDay();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let nowDate = date.getDate();
+
+  if (hours < 10) {
+    hours = `0${hour}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let actualDay = days[day];
+  let dayTime = document.querySelector("#current-date");
+  dayTime.innerHTML = `${actualDay} ${nowDate}, ${hours}:${minutes}`;
+}
+
 let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+let dateUpdate = document.querySelector("#search-form");
+dateUpdate.addEventListener("submit", updateDate);
